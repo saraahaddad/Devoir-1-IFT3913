dd1140c38f3e3bee77e2fd255850b2751dea3aac
saraahaddad <saraa.haddad@icloud.com>
2023-09-25 15:48:28 -0400

    Commit 1: Exercice 1 done + test file

---
+++ src/TlocCounter.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TlocCounter {
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length != 1){
+            System.out.println("Error! Please enter the correct number of arguments");
+        }
+        String fileName = args[0];
+        computeTloc(fileName);
+    }
+
+    private static void computeTloc(String fileName) throws FileNotFoundException {
+        try {
+            File file = new File(fileName);
+            Scanner fileReader = new Scanner(file);
+            int linesOfCode = 0;
+
+            while (fileReader.hasNext()) {
+                String line = fileReader.nextLine();
+                if (!line.startsWith("//") && !line.isEmpty()) {
+                    linesOfCode++;
+                }
+            }
+
+            fileReader.close();
+            System.out.println(linesOfCode);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Oops! An error occured");
+            e.printStackTrace();
+        }
+    }
+}
